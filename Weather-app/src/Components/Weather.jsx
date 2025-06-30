1064cc79d0847f48261fc6a548a64efc45bd0d88
Ananya Sharma <ananyasharma3064@gmail.com>
2025-06-29 23:08:04 -0400

    worked on the UI

---
+++ Weather-app/src/Components/Weather.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Weather.css'
+const Weather  = () => {
+  return (
+    <div className="weather">
+        <div className="searchbar">
+            <input type="text" placeholder='search'/><i>🔍</i>
+        </div>
+    </div>
+  )
+}
+
+export default  Weather
+ 
