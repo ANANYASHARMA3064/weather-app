7eebbf53c4c56c0f9ded80c271ec0a8723a409c7
Ananya Sharma <ananyasharma3064@gmail.com>
2025-07-01 15:53:04 -0400

    worked on the UI

--- Weather-app/src/Components/Weather.jsx
+++ Weather-app/src/Components/Weather.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import './Weather.css'
+import search_icon from '../assets/search.png'
+import clear from '../assets/clear.png'
+import cloud from '../assets/cloud.png'
+import drizzle from '../assets/drizzle.png'
+import humidity from '../assets/humidity.png'
+import snow from '../assets/snow.png'
+import windy from '../assets/windy.png'
 const Weather  = () => {
   return (
     <div className="weather">
         <div className="searchbar">
-            <input type="text" placeholder='search'/><i>🔍</i>
+            <input type="text" placeholder='search'/>
+            <img id="search" src={search_icon}/>
+           
         </div>
+        <img src={clear} alt="" className='weather-  icon'></img>
+        <p>16°</p>
+        <p>London</p>
+        
     </div>
   )
 }
